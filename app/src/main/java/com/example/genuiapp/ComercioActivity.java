156afea84b984ff221ed4484205db8912d8addc4
hernan151474 <104164534+hernan151474@users.noreply.github.com>
2022-06-02 20:41:32 -0300

    empezando con la segunda pantalla de oficios

---
+++ app/src/main/java/com/example/genuiapp/ComercioActivity.java
@@ -0,0 +1,14 @@
+package com.example.genuiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComercioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comercio);
+    }
+}

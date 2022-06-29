5b9be215092bc86daa8b60aba2db0e639587d5e1
hernan151474 <38471822@populorumjujuy.ar>
2022-06-29 12:59:39 -0300

    Agregado de pantalla que hacemos completa

---
+++ app/src/main/java/com/example/genuiapp/Que_Hacemos.java
@@ -0,0 +1,14 @@
+package com.example.genuiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Que_Hacemos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_que_hacemos);
+    }
+}
